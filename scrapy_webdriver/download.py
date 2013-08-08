991c3a19540c68ac69e565961124b900289fd06f
Mike Stringer <mike.stringer@datascopeanalytics.com>
2013-08-08 14:16:38 -0500

    more testing on hang timeout stuff in download.py

--- scrapy_webdriver/download.py
+++ scrapy_webdriver/download.py
@@ -21,6 +21,7 @@ class WebdriverDownloadHandler(object):
     def __init__(self, settings):
         self._enabled = settings.get('WEBDRIVER_BROWSER') is not None
         self._timeout = settings.get('WEBDRIVER_TIMEOUT')
+        self._hang_timeout = settings.get('WEBDRIVER_HANG_TIMEOUT', None)
         self._fallback_handler = load_object(FALLBACK_HANDLER)(settings)
 
     def download_request(self, request, spider):
@@ -28,25 +29,27 @@ class WebdriverDownloadHandler(object):
         if self._enabled and isinstance(request, WebdriverRequest):
 
             # set the signal handler for the SIGALRM event
-            def handler(signum, frame):
+            if self._hang_timeout:
 
-                # kill the selenium webdriver process (with SIGTERM,
-                # so that it kills both the primary process and the
-                # process that gets spawned)
-                request.manager.webdriver.service.process.send_signal(signal.SIGTERM)
+                def alarm_handler(signum, frame):
 
-                # set the defunct _webdriver attribute back to
-                # original value of None, so that the next time it is
-                # accessed it is recreated.
-                request.manager._webdriver = None
+                    # kill the selenium webdriver process (with SIGTERM,
+                    # so that it kills both the primary process and the
+                    # process that gets spawned)
+                    request.manager.webdriver.service.process.send_signal(signal.SIGTERM)
 
-                # log an informative warning message
-                msg = "'webdriver.get' for '%s' took more than %s seconds." % \
-                    (request.url, self._timeout)
-                spider.log(msg, level=log.WARNING)
+                    # set the defunct _webdriver attribute back to
+                    # original value of None, so that the next time it is
+                    # accessed it is recreated.
+                    request.manager._webdriver = None
 
-            # bind the handler
-            signal.signal(signal.SIGALRM, handler)
+                    # log an informative warning message
+                    msg = "WebDriver.get for '%s' took more than WEBDRIVER_HANG_TIMEOUT (%ss)" % \
+                        (request.url, self._hang_timeout)
+                    spider.log(msg, level=log.INFO)
+
+                # bind the handler
+                signal.signal(signal.SIGALRM, alarm_handler)
 
             if isinstance(request, WebdriverActionRequest):
                 download = self._do_action_request
@@ -59,11 +62,11 @@ class WebdriverDownloadHandler(object):
     @inthread
     def _download_request(self, request, spider):
         """Download a request URL using webdriver."""
-        log.msg('Downloading %s with webdriver' % request.url, level=log.DEBUG)
+        spider.log('Downloading %s with webdriver' % request.url, level=log.DEBUG)
 
         # set a countdown timer for the webdriver.get
-        if self._timeout:
-            signal.alarm(self._timeout)
+        if self._hang_timeout:
+            signal.alarm(self._hang_timeout)
 
         # make the get request
         try:
@@ -72,14 +75,37 @@ class WebdriverDownloadHandler(object):
         # if the get fails for any reason, set the webdriver attribute of the
         # response to the exception that occurred
         except Exception, exception:
+
+            # since it's already failed, don't try to raise alarm anymore (this has no effect if the failure was due to the alarm)
+            if self._hang_timeout:
+                spider.log('settings alarm to 0 on FAILURE', level=log.DEBUG)
+                spider.log('FAIL: ' + str(request.manager._webdriver), level=log.DEBUG)
+                signal.alarm(0)
+
+            # set page_source to blank so that WebdriverResponse doesn't complain
             exception.page_source = '<html><head></head><body></body></html>'
+
+            # log a nice error message
+            msg = 'Error while downloading %s with webdriver (%s)' % \
+                (request.url, exception)
+            spider.log(msg, level=log.ERROR)
+
+            # since manager.webdriver is a @property, this will recreate connection
+            webdriver = request.manager.webdriver
+            spider.log('FAIL 2. THIS SHOULD BE WEBDRIVER: ' + str(request.manager._webdriver), level=log.DEBUG)
             return WebdriverResponse(request.url, exception)
 
         # if the get finishes, defuse the bomb and return a response with the
         # webdriver attached
         else:
-            if self._timeout:
+
+            # since it succeeded, don't raise any alarm
+            if self._hang_timeout:
+                spider.log('settings alarm to 0 on SUCCESS', level=log.DEBUG)
+                spider.log('YEAH: ' + str(request.manager._webdriver), level=log.DEBUG)
                 signal.alarm(0)
+
+            # return the correct response
             return WebdriverResponse(request.url, request.manager.webdriver)
             
     @inthread
